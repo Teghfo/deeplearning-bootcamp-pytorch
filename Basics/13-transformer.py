a8a955ed911bb6d14135fa84dd5d385c3ca08ede
teghfo <teghfo@gmail.com>
2024-03-15 13:56:21 +0330

    W10: positional Encoding

--- Basics/13-transformer.py
+++ Basics/13-transformer.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import math
 
+
 # x = torch.arange(0, 12).view(1, 3, 4)
 # print(x)
 # print(x.transpose(-2, -1))
@@ -69,9 +70,20 @@ class PositionalEncoding(nn.Module):
     def __init__(self, max_len, d_model) -> None:
         super().__init__()
         self.d_model = d_model
+        pe = torch.zeros(max_len, 1, d_model)
+        position = torch.arange(0, max_len).float().unsqueeze(1)
+        step_compnent = torch.arange(0, d_model, 2).float()
+        w = torch.exp(step_compnent * (-math.log(10_000) / d_model))
+
+        pe[:, 0, 0::2] = torch.sin(w * position)
+        pe[:, 0, 1::2] = torch.cos(w * position)
+
+        self.register_buffer("pe", pe)
 
     def forward(self, x):
-        pass
+
+        # batch first false
+        return x + self.pe[:x.size(0)]
 
 
 class EncoderBlock(nn.Module):
@@ -93,6 +105,32 @@ class EncoderBlock(nn.Module):
         return x
 
 
+class DecoderBlock:
+    def __init__(self, d_model, num_heads, d_ff, dropout=0.1, src_mask=None, trg_mask=None, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.multi_head_self_atten = MultiHead(d_model, num_heads, src_mask)
+        self.multi_head_cross_atten = MultiHead(d_model, num_heads, trg_mask)
+        self.layer_norm1 = nn.LayerNorm(d_model)
+        self.layer_norm2 = nn.LayerNorm(d_model)
+        self.layer_norm3 = nn.LayerNorm(d_model)
+        self.dropout = nn.Dropout(dropout)
+        self.ff = PositionWiseFeedForwardNetworks(d_model, d_ff, self.dropout)
+
+    def forward(self, trg: torch.Tensor, src: torch.Tensor, ):
+        pass
+
+
+class Transformer(nn.Module):
+    def __init__(self, d_model: int, src_embed_size: int,  trg_embed_size: int, num_head: int = 8, num_layers: int = 6, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.embed_enc = nn.Embedding(src_embed_size, d_model)
+        self.embed_dec = nn.Embedding(trg_embed_size, d_model)
+
+        # TODO: complete!
+    def generate_mask(self):
+        self
+
+
 # model = MultiHead()
 # x = torch.randn(40, 32, 512)
 # print(model(x, x, x).size())
@@ -105,6 +143,12 @@ class EncoderBlock(nn.Module):
 # print(linear(z).view(2, 10, 10))
 
 
-model = EncoderBlock(512, 8, 2048, dropout=0.2)
-x = torch.randn(40, 32, 512)
-print(model(x).size())
+#
+# model = EncoderBlock(512, 8, 2048, dropout=0.2)
+# x = torch.randn(40, 32, 512)
+# print(model(x).size())
+seq_len = 40
+d_model = 512
+x = torch.randn(seq_len, 32, d_model)
+pe = PositionalEncoding(100, d_model)
+print(pe(x))
